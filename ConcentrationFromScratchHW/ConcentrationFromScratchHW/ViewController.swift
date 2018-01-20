24429dc96008869f673cc4fa03ae430232069c20
knail1 <oansari@gmail.com>
2018-01-20 09:02:07 -0600

    creating app from scratch, and updating final notes on lecture 2 in concentration/

---
+++ ConcentrationFromScratchHW/ConcentrationFromScratchHW/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  basically i'm attempting to write the concentration app
+//  from scratch!
+//  ViewController.swift
+//  ConcentrationFromScratchHW
+//
+//  Created by knail1 on 1/18/18.
+//  Copyright © 2018 knail1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var cardCollection: UIButton!
+    
+    
+    @IBAction func clickOnCard(_ sender: UIButton) {
+    }
+    
+}
+
